14f12fcd2ed4f70a48a07737ddb9ac47dae39038
User <elmuratuulua@gmail.com>
2022-07-28 17:25:31 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Triangle triangle =new Triangle();
+        triangle.base = 12;
+        triangle.height = 16;
+        triangle.hypethesis = 20;
+        triangle.area();
+        triangle.perimetr();
+
+    }
+}
---
+++ src/Triangle.java
@@ -0,0 +1,15 @@
+public class Triangle {
+    int base;
+    int height;
+    int hypethesis;
+
+    void area(){
+        System.out.print("Area  of triangle is ");
+        System.out.println((base*height)/2);
+    }
+    void perimetr(){
+        System.out.print("perimetr of triangle is ");
+        System.out.println(base + height + hypethesis);
+    }
+
+}
